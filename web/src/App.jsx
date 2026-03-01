51171721ae525a009fa61d943a700dabd40252a3
Aadit Sachdev <A.Sachdev-1@student.tudelft.nl>
2026-03-02 00:56:25 +0100

    Added persistent storage for added code

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,6 +4,8 @@ import HomeView from './components/home/HomeView'
 import EditorView from './components/editor/EditorView'
 import SimulatorView from './components/simulator/SimulatorView'
 
+const STORAGE_KEY = 'asm_sim_code'
+
 const PLACEHOLDER =
     '# Example code\n' +
     '# This moves 0 to %rdi, signalling a successful exit\n' +
@@ -14,9 +16,28 @@ const PLACEHOLDER =
     '  movq $0, %rdi\n' +
     '  call exit\n'
 
+function loadCode() {
+    try {
+        return localStorage.getItem(STORAGE_KEY) ?? PLACEHOLDER
+    } catch {
+        return PLACEHOLDER
+    }
+}
+
+function saveCode(code) {
+    try {
+        localStorage.setItem(STORAGE_KEY, code)
+    } catch {}
+}
+
 export default function App() {
-    const [code, setCode] = useState(PLACEHOLDER)
+    const [code, setCode] = useState(loadCode)
     const [routerKey, setRouterKey] = useState(0)
+
+    const handleCodeChange = useCallback((newCode) => {
+        setCode(newCode)
+        saveCode(newCode)
+    }, [])
 
     const forceNavigate = useCallback((path) => {
         window.history.replaceState(null, '', path)
@@ -27,7 +48,7 @@ export default function App() {
         <BrowserRouter key={routerKey}>
             <Routes>
                 <Route path="/"          element={<HomeView />} />
-                <Route path="/editor"    element={<EditorView code={code} onCodeChange={setCode} />} />
+                <Route path="/editor"    element={<EditorView code={code} onCodeChange={handleCodeChange} />} />
                 <Route path="/simulator" element={<SimulatorView code={code} forceNavigate={forceNavigate} />} />
                 <Route path="*"          element={<Navigate to="/" replace />} />
             </Routes>
